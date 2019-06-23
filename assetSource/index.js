041e9cbd574a7679008d78484707cd79ccb00008
maxim <wertfani@mail.ru>
2019-06-23 23:42:01 +0700

    review

--- assetSource/index.js
+++ assetSource/index.js
@@ -6,3 +6,9 @@ import { App } from 'App';
 const root = document.querySelector('#root');
 
 ReactDOM.render(<App />, root);
+
+if (module.hot) {
+    module.hot.accept('App', () => {
+        ReactDOM.render(<App />, root);
+    });
+}
