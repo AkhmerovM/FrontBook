cfbc821e350f408a11d0e9870848b83cdb6df572
Akhmerov <m.akhmerov@dev.userstory.ru>
2019-06-24 09:53:15 +0700

    meteor

---
+++ frontbook/client/main.js
@@ -0,0 +1,31 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import './main.html';
+import {React} from 'react';
+import ReactDOM from 'react-dom';
+import { App } from 'assetSource/App';
+
+// Template.hello.onCreated(function helloOnCreated() {
+//   // counter starts at 0
+//   this.counter = new ReactiveVar(0);
+// });
+//
+// Template.hello.helpers({
+//   counter() {
+//     return Template.instance().counter.get();
+//   },
+// });
+//
+// Template.hello.events({
+//   'click button'(event, instance) {
+//     // increment the counter when button is clicked
+//     instance.counter.set(instance.counter.get() + 1);
+//   },
+// });
+
+const root = document.querySelector('#root');
+
+
+Meteor.startup(() => {
+  ReactDOM.render(<App />, root);
+});
